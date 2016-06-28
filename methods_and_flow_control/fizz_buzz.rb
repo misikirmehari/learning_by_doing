3f97db0a173b954953a90db169e80a526fa1bd72
Misikir <misikirmehari@gmail.com>
2016-06-28 16:34:09 -0600

    Fixed rubocop errors

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 1.upto(100) do |number|
   if number % 5 == 0 && number % 3 == 0
     puts 'FizzBuzz'
@@ -9,9 +8,5 @@
     puts 'Fizz'
   else
     puts number
-
-
   end
 end
-
-
